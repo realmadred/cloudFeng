e8beb998295f8005ab0f19d3553cebd042ceb5ea
realmadred <liufeng2008168@163.com>
2018-03-05 14:37:05 +0800

    配置文件

--- servera/src/main/java/com/feng/controller/HelloController.java
+++ servera/src/main/java/com/feng/controller/HelloController.java
@@ -1,7 +1,10 @@
 package com.feng.controller;
 
 import com.feng.client.ServiceBClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,12 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello")
 public class HelloController {
 
+    private final static Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
+
     @Autowired
     ServiceBClient bClient;
 
+    @Value("${name}")
+    private String name;
+
+    @Value("${age}")
+    private String age;
+
     @RequestMapping("/say")
     public String say(){
-        System.out.println("say...");
+        LOGGER.info("say name:{},age{}",name,age);
         return "hello";
     }
 
