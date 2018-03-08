8c1cdcc984122190a08edd5403af6d75ef1f5322
realmadred <liufeng2008168@163.com>
2018-03-08 17:33:24 +0800

    jwt

--- auth/src/main/java/com/feng/auth/config/OAuthConfiguration.java
+++ auth/src/main/java/com/feng/auth/config/OAuthConfiguration.java
@@ -1,5 +1,6 @@
 package com.feng.auth.config;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,7 +14,11 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.util.Assert;
 
 import javax.sql.DataSource;
 
@@ -34,6 +39,18 @@ public class OAuthConfiguration extends AuthorizationServerConfigurerAdapter {
         return new JdbcTokenStore(dataSource);
     }
 
+    @Bean
+    public TokenStore jwtTokenStore() {
+        return new JwtTokenStore(jwtAccessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+        JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
+        jwtAccessTokenConverter.setSigningKey("870978098sdfoaijhlkjlkdsf");
+        return jwtAccessTokenConverter;
+    }
+
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security)
             throws Exception {
@@ -44,7 +61,8 @@ public class OAuthConfiguration extends AuthorizationServerConfigurerAdapter {
     public void configure(AuthorizationServerEndpointsConfigurer endpoints)
             throws Exception {
         endpoints.authenticationManager(auth)
-                .tokenStore(tokenStore());
+                .tokenStore(jwtTokenStore())
+                .accessTokenConverter(jwtAccessTokenConverter());
     }
 
     @Override
