c9b014b802fb2f70579de8361d0d71477c169e49
realmadred <liufeng2008168@163.com>
2020-05-04 00:06:55 +0800

    dubbo

--- dubbo-use-service/src/main/java/com/feng/service/SpringRestService.java
+++ dubbo-use-service/src/main/java/com/feng/service/SpringRestService.java
@@ -1,78 +1,78 @@
-package com.feng.service;
-
-import com.feng.api.RestService;
-import com.feng.dto.UserDto;
-import org.apache.dubbo.config.annotation.Service;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
-import java.util.Map;
-
-/**
- * Spring MVC {@link RestService}.
- *
- * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
- */
-@Service(version = "1.0.0")
-@RestController
-public class SpringRestService implements RestService {
-
-	@Override
-	@GetMapping("/param")
-	public String param(@RequestParam String param) {
-		return param;
-	}
-
-	@Override
-	@PostMapping("/params")
-	public String params(@RequestParam int a, @RequestParam String b) {
-		return a + b;
-	}
-
-	@Override
-	@GetMapping("/headers")
-	public String headers(@RequestHeader("h") String header,
-			@RequestHeader("h2") String header2, @RequestParam("v") Integer param) {
-		String result = header + " , " + header2 + " , " + param;
-		return result;
-	}
-
-	@Override
-	@GetMapping("/path-variables/{p1}/{p2}")
-	public String pathVariables(@PathVariable("p1") String path1,
-			@PathVariable("p2") String path2, @RequestParam("v") String param) {
-		String result = path1 + " , " + path2 + " , " + param;
-		return result;
-	}
-
-	@Override
-	@PostMapping("/form")
-	public String form(@RequestParam("f") String form) {
-		return String.valueOf(form);
-	}
-
-	@Override
-	@PostMapping(value = "/request/body/map",
-			produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	public UserDto requestBodyMap(@RequestBody Map<String, Object> data,
-								  @RequestParam("param") String param) {
-		UserDto user = new UserDto();
-		user.setId(((Integer) data.get("id")).longValue());
-		user.setName((String) data.get("name"));
-		user.setAge((Integer) data.get("age"));
-		return user;
-	}
-
-	@PostMapping(value = "/request/body/user",
-			consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	@Override
-	public Map<String, Object> requestBodyUser(@RequestBody UserDto user) {
-		Map<String, Object> map = new HashMap<>();
-		map.put("id", user.getId());
-		map.put("name", user.getName());
-		map.put("age", user.getAge());
-		return map;
-	}
-
-}
+//package com.feng.service;
+//
+//import com.feng.api.RestService;
+//import com.feng.dto.UserDto;
+//import org.apache.dubbo.config.annotation.Service;
+//import org.springframework.http.MediaType;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//
+///**
+// * Spring MVC {@link RestService}.
+// *
+// * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
+// */
+//@Service(version = "1.0.0")
+//@RestController
+//public class SpringRestService implements RestService {
+//
+//	@Override
+//	@GetMapping("/param")
+//	public String param(@RequestParam String param) {
+//		return param;
+//	}
+//
+//	@Override
+//	@PostMapping("/params")
+//	public String params(@RequestParam int a, @RequestParam String b) {
+//		return a + b;
+//	}
+//
+//	@Override
+//	@GetMapping("/headers")
+//	public String headers(@RequestHeader("h") String header,
+//			@RequestHeader("h2") String header2, @RequestParam("v") Integer param) {
+//		String result = header + " , " + header2 + " , " + param;
+//		return result;
+//	}
+//
+//	@Override
+//	@GetMapping("/path-variables/{p1}/{p2}")
+//	public String pathVariables(@PathVariable("p1") String path1,
+//			@PathVariable("p2") String path2, @RequestParam("v") String param) {
+//		String result = path1 + " , " + path2 + " , " + param;
+//		return result;
+//	}
+//
+//	@Override
+//	@PostMapping("/form")
+//	public String form(@RequestParam("f") String form) {
+//		return String.valueOf(form);
+//	}
+//
+//	@Override
+//	@PostMapping(value = "/request/body/map",
+//			produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+//	public UserDto requestBodyMap(@RequestBody Map<String, Object> data,
+//								  @RequestParam("param") String param) {
+//		UserDto user = new UserDto();
+//		user.setId(((Integer) data.get("id")).longValue());
+//		user.setName((String) data.get("name"));
+//		user.setAge((Integer) data.get("age"));
+//		return user;
+//	}
+//
+//	@PostMapping(value = "/request/body/user",
+//			consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+//	@Override
+//	public Map<String, Object> requestBodyUser(@RequestBody UserDto user) {
+//		Map<String, Object> map = new HashMap<>();
+//		map.put("id", user.getId());
+//		map.put("name", user.getName());
+//		map.put("age", user.getAge());
+//		return map;
+//	}
+//
+//}
--- dubbo-use-service/src/main/java/com/feng/service/StandardRestService.java
+++ dubbo-use-service/src/main/java/com/feng/service/StandardRestService.java
@@ -1,123 +1,124 @@
-///*
-// * Copyright 2013-2018 the original author or authors.
-// *
-// * Licensed under the Apache License, Version 2.0 (the "License");
-// * you may not use this file except in compliance with the License.
-// * You may obtain a copy of the License at
-// *
-// *      https://www.apache.org/licenses/LICENSE-2.0
-// *
-// * Unless required by applicable law or agreed to in writing, software
-// * distributed under the License is distributed on an "AS IS" BASIS,
-// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// * See the License for the specific language governing permissions and
-// * limitations under the License.
-// */
-//
-//package com.feng.service;
-//
-//import java.util.HashMap;
-//import java.util.Map;
-//
-//import javax.ws.rs.Consumes;
-//import javax.ws.rs.FormParam;
-//import javax.ws.rs.GET;
-//import javax.ws.rs.HeaderParam;
-//import javax.ws.rs.POST;
-//import javax.ws.rs.Path;
-//import javax.ws.rs.PathParam;
-//import javax.ws.rs.Produces;
-//import javax.ws.rs.QueryParam;
-//import javax.ws.rs.core.MediaType;
-//
-//import com.feng.api.RestService;
-//import com.feng.dto.UserDto;
-//import lombok.extern.slf4j.Slf4j;
-//import org.apache.dubbo.config.annotation.Service;
-//
-//import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
-//
-///**
-// * Default {@link RestService}.
-// *
-// * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
-// */
-//@Service(version = "1.0.0", protocol = { "dubbo", "rest" })
-//@Path("/")
-//@Slf4j
-//public class StandardRestService implements RestService {
-//
-//
-//	@Override
-//	@Path("param")
-//	@GET
-//	public String param(@QueryParam("param") String param) {
-//		log.info("/param:{}", param);
-//		return param;
-//	}
-//
-//	@Override
-//	@Path("params")
-//	@POST
-//	public String params(@QueryParam("a") int a, @QueryParam("b") String b) {
-//		log.info("/params:{}", a + b);
-//		return a + b;
-//	}
-//
-//	@Override
-//	@Path("headers")
-//	@GET
-//	public String headers(@HeaderParam("h") String header,
-//			@HeaderParam("h2") String header2, @QueryParam("v") Integer param) {
-//		String result = header + " , " + header2 + " , " + param;
-//		log.info("/headers:{}", result);
-//		return result;
-//	}
-//
-//	@Override
-//	@Path("path-variables/{p1}/{p2}")
-//	@GET
-//	public String pathVariables(@PathParam("p1") String path1,
-//			@PathParam("p2") String path2, @QueryParam("v") String param) {
-//		String result = path1 + " , " + path2 + " , " + param;
-//		log.info("/path-variables:{}", result);
-//		return result;
-//	}
-//
-//	// @CookieParam does not support : https://github.com/OpenFeign/feign/issues/913
-//	// @CookieValue also does not support
-//
-//	@Override
-//	@Path("form")
-//	@POST
-//	public String form(@FormParam("f") String form) {
-//		return String.valueOf(form);
-//	}
-//
-//	@Override
-//	@Path("request/body/map")
-//	@POST
-//	@Produces(APPLICATION_JSON_VALUE)
-//	public UserDto requestBodyMap(Map<String, Object> data,
-//								  @QueryParam("param") String param) {
-//		UserDto user = new UserDto();
-//		user.setId(((Integer) data.get("id")).longValue());
-//		user.setName((String) data.get("name"));
-//		user.setAge((Integer) data.get("age"));
-//		log.info("/request/body/map:{}", param);
-//		return user;
-//	}
-//
-//	@Path("request/body/user")
-//	@POST
-//	@Override
-//	@Consumes(MediaType.APPLICATION_JSON)
-//	public Map<String, Object> requestBodyUser(UserDto user) {
-//		Map<String, Object> map = new HashMap<>();
-//		map.put("id", user.getId());
-//		map.put("name", user.getName());
-//		map.put("age", user.getAge());
-//		return map;
-//	}
-//
-//}
+/*
+ * Copyright 2013-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.feng.service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import com.feng.api.RestService;
+import com.feng.dto.UserDto;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.dubbo.config.annotation.Service;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
+
+/**
+ * Default {@link RestService}.
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
+ */
+@Service(version = "1.0.0", protocol = { "dubbo", "rest" })
+@Path("/")
+@Slf4j
+public class StandardRestService implements RestService {
+
+
+	@Override
+	@Path("param")
+	@GET
+	public String param(@QueryParam("param") String param) {
+		log.info("/param:{}", param);
+		return param;
+	}
+
+	@Override
+	@Path("params")
+	@POST
+	public String params(@QueryParam("a") int a, @QueryParam("b") String b) {
+		log.info("/params:{}", a + b);
+		return a + b;
+	}
+
+	@Override
+	@Path("headers")
+	@GET
+	public String headers(@HeaderParam("h") String header,
+			@HeaderParam("h2") String header2, @QueryParam("v") Integer param) {
+		String result = header + " , " + header2 + " , " + param;
+		log.info("/headers:{}", result);
+		return result;
+	}
+
+	@Override
+	@Path("path-variables/{p1}/{p2}")
+	@GET
+	public String pathVariables(@PathParam("p1") String path1,
+			@PathParam("p2") String path2, @QueryParam("v") String param) {
+		String result = path1 + " , " + path2 + " , " + param;
+		log.info("/path-variables:{}", result);
+		return result;
+	}
+
+	// @CookieParam does not support : https://github.com/OpenFeign/feign/issues/913
+	// @CookieValue also does not support
+
+	@Override
+	@Path("form")
+	@POST
+	public String form(@FormParam("f") String form) {
+		return String.valueOf(form);
+	}
+
+	@Override
+	@Path("request/body/map")
+	@POST
+	@Produces(APPLICATION_JSON_VALUE)
+	public UserDto requestBodyMap(Map<String, Object> data,
+								  @QueryParam("param") String param) {
+		UserDto user = new UserDto();
+		user.setId(((Integer) data.get("id")).longValue());
+		user.setName((String) data.get("name"));
+		user.setAge((Integer) data.get("age"));
+		log.info("/request/body/map:{}", param);
+		return user;
+	}
+
+	@Path("request/body/user")
+	@POST
+	@Override
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Map<String, Object> requestBodyUser(UserDto user) {
+		Map<String, Object> map = new HashMap<>();
+		map.put("id", user.getId());
+		map.put("name", user.getName());
+		map.put("age", user.getAge());
+		return map;
+	}
+
+}
